608ece25d568c810d71760b4f54c112134e66883
myishatunsamiha <myisha.samiha@gmail.com>
2022-05-07 04:27:20 +0600

    created genius car server

---
+++ _steps.js
@@ -0,0 +1,11 @@
+/**
+ * 1. create a folder
+ * 2. npm init
+ * 3. npm install express cors mongodb dotenv
+ * 4. create .env file in the root folder 
+ * 5. inside package.json,
+ *   "scripts": {
+    "start": "node index.js",
+    "start-dev": "nodemon index.js",}
+*
+*/
